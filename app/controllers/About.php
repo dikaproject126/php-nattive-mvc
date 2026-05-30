52dc213e88cd9fa1b56fe70aec5b00d0c945ae8b
projectdika <projectdika88@gmail.com>
2026-05-30 23:02:12 +0800

    first commit

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php 
+
+class About{
+    public function index($nama ='Dika', $impian = 'Investor'){
+        echo "Halo, nama saya $nama. Impian saya ingin menjadi $impian";
+    }
+
+    public function page(){
+        echo 'About/page';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once '../app/init.php'; //teknik bootstraping
+
+
+$app = new App;
