df1b365610619720757ef05ea5d0d39ae90bfa76
dikaproject126 <dikaproject126@gmail.com>
2026-06-01 14:53:54 +0800

    fix : create db and connect tu model Mahasiswa_model

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,8 +3,9 @@
 class Home extends Controller{ 
     public function index(){
         $data['judul'] = 'Home';
+        $data['nama'] = $this->model('User_model')->getUser();
         $this->view('layouts/header', $data);
-        $this->view('home/index');
+        $this->view('home/index', $data);
         $this->view('layouts/footer');
     }
 }
---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model{
+    private $nama = 'Dika Mahendra';
+
+    public function getUser(){
+        return $this->nama;
+    }
+}
